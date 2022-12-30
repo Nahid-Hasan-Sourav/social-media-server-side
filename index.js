8bda0ea793fe76b8ff60e84384add8c597bd2d25
Nahid-Hasan_Sourav <nahidhasansourav05@gmail.com>
2022-12-31 04:19:23 +0600

    user added in fb

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express=require('express');
+const jwt = require('jsonwebtoken');
 const cors=require('cors');
 const app=express();
 const port=process.env.PORT || 5000;
@@ -16,18 +17,35 @@ console.log(process.env.DB_USER);
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ehiaece.mongodb.net/?retryWrites=true&w=majority`;
-
-console.log(uri)
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+// https://social-media-server-side.vercel.app
 
 async function run(){
     try{
         const userCollection=client.db("socialDb").collection("allUser");
+
+        // SAVE USER INFO AND GENERATE A TOKEN
+        app.put('/user/:email', async(req,res)=>{
+            const email=req.params.email
+            const userInfo=req.body;
+            const filter={email:email}
+            const options={upsert:true}
+            const updateDoc={
+                $set:userInfo,
+            }
+
+            const result=await userCollection.updateOne(filter,updateDoc,options);
+            console.log("Save user information :",result);
+            const token=jwt.sign(userInfo,process.env.ACCESS_TOKEN_SECRET,{expiresIn:'1d'})
+            console.log("Token :",token)
+            res.send({result,token})
+
+        })
+
     }
-    catch(err){
-        console.log(err);
+    finally{
+       
     }
 
     
@@ -38,7 +56,7 @@ run()
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('This is social server-app server');
 })
 
 app.listen(port, () => {
